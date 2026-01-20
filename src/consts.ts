810b96549228c39d6d6e513bb4953e56dcbe8c90
joelrodriguezgzz <148912366+joelrodriguezgzz@users.noreply.github.com>
2026-01-20 16:41:41 -0600

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Astro + Cloudflare Demo';
+export const SITE_DESCRIPTION = 'Edge-first web architecture with Astro and Cloudflare.';
